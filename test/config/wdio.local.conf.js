d3ba760e4df89ebd294986133e970bc4a65f55f1
Chandra Pal Singh Parihar <cparihar@altimetrik.com>
2022-03-26 23:14:07 +0530

    adding allure reporting

--- test/config/wdio.local.conf.js
+++ test/config/wdio.local.conf.js
@@ -241,8 +241,11 @@ exports.config = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+     afterTest: function(test, context, { error, result, duration, passed, retries }) {
+         if(error){
+             browser.takeScreenshot();
+         }
+     },
 
 
     /**
