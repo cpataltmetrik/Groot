1b46f75f80c0bbc179a4bd002afbc2a2464e8787
Chandra Pal Singh Parihar <cparihar@altimetrik.com>
2022-04-04 14:55:25 +0530

    removed js config file

--- test/config/wdio.local.conf.ts
+++ test/config/wdio.local.conf.ts
@@ -159,11 +159,15 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['spec',
 
-
-    
-    //
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: true,
+            disableWebdriverScreenshotsReporting: false,
+        }
+        ]],    
+        
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
