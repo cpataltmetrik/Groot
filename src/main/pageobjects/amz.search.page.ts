5a9c574e50fc7be6edead56b0135b4e41481790b
njerfia <102955928+njerfia@users.noreply.github.com>
2022-04-19 15:58:13 +0530

    Committing tsconfig file for d.ts file deletion related changes

--- src/main/pageobjects/amz.search.page.ts
+++ src/main/pageobjects/amz.search.page.ts
@@ -13,15 +13,15 @@ class SearchPage extends Page {
     }
 
     public get btnSearch() {
-        return $('#nav-search-submit-button');
+        return <any>$('#nav-search-submit-button');
     }
 
     public get searchResults() {
-        return $$('//div/h2/a/span[contains(text(),"Apple iPhone 12")]');
+        return <any>$$('//div/h2/a/span[contains(text(),"Apple iPhone 12")]');
     }
 
     public get amazonChoiceList() {
-        return $$('//span[text()="Amazon\'s "]/following::span[1][text()="Choice"]');
+        return <any>$$('//span[text()="Amazon\'s "]/following::span[1][text()="Choice"]');
     }
 
     /**
