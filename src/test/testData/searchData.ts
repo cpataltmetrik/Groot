6084d78ba6448773f26847922df8123f3cbfd6ca
njerfia <102955928+njerfia@users.noreply.github.com>
2022-04-20 13:59:04 +0530

    Updated review comments & Updated the folder structure and its associated path in files

--- src/test/testData/searchData.ts
+++
@@ -1,8 +0,0 @@
-const SEARCH_DATA = {
-    dataset1: {
-      searchString: 'iPhone 12',
-      expectedString: 'Apple'
-    }
-}
-
-export default SEARCH_DATA
--- utilities/logger.ts
+++
@@ -1,6 +0,0 @@
-import allureReporter from '@wdio/allure-reporter';
-
-export const addLogger = (log: string) => {
-     allureReporter.addStep(`STEP: ${log}`)
-     console.log(`STEP: ${log}`)
-}
