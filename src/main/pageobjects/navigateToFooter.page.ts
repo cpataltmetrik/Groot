0865ff98c76b684580b542758cc1e27abe22aa38
spoorthy N <nagaraja.spoorthy@gosite.com>
2022-04-25 17:30:48 +0530

    Renamed the files as per naming covention doc

---
+++ src/main/pageobjects/navigateToFooter.page.ts
@@ -0,0 +1,31 @@
+import Page from "./page";
+const assertchai = require("chai").assert;
+import * as config from "config";
+import { isAssertClause, OperationCanceledException } from "typescript";
+
+export default class Footer extends Page {
+  public get helpButton() {
+    return <any>$("//a[.='Amazon Assistant Download']/following::a[.='Help']");
+  }
+  public get searchText() {
+    return <any>$("//input[@type='search']");
+  }
+  public get searchButton() {
+    return <any>$("//i[@class='a-icon a-icon-search']");
+  }
+  public get amazonPayKYC() {
+    return <any>$("(//a[@class='a-link-normal'])[1]");
+  }
+  public async navigateToFooter() {
+    await this.helpButton.scrolltoView();
+    await this.helpButton.clickWhenDisplayed();
+  }
+  public async amazonPayLinksValidation(text: any) {
+    await await this.searchText.setValue(text);
+    await this.searchText.keys("Enter");
+    assertchai.exists(await this.amazonPayKYC);
+  }
+  public open() {
+    return super.open("");
+  }
+}
