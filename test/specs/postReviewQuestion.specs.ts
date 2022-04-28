2b393f26702a53b3a6ba985f7417bc30c580b40c
spoorthy N <nagaraja.spoorthy@gosite.com>
2022-04-28 12:07:22 +0530

    pushing post_review_questions script

--- test/specs/postReviewQuestion.specs.ts
+++ test/specs/postReviewQuestion.specs.ts
@@ -1,34 +1,24 @@
 import * as config from "config";
-import { addLogger } from '../../src/main/utilities/logger'
-import postReviewQuestion from "../../src/main/pageobjects/postReviewQuestion.page"
-const postReviewQuestions=new postReviewQuestion();
+import { addLogger } from "../../src/main/utilities/logger";
+import postReviewQuestion from "../../src/main/pageobjects/postReviewQuestion.page";
+const postReviewQuestions = new postReviewQuestion();
 const ASSERT_CHAI = require("chai").assert;
 
-
-describe("Post a Review Question on Amazon Product and validate the Same ",async()=>
-{ 
-    it("Post a Review Question on Amazon Product and validate the Same ",async()=>
-    {
-       postReviewQuestions.open();
-       postReviewQuestions.searchProduct("mobiles");
-       //ASSERT_CHAI.exists(await postReviewQuestions.selectMobile);
-       browser.pause(5000)
-       await postReviewQuestions.selectMobile.waitTillInterval(5000)
-       let handles:any=await browser.getWindowHandles();
-       await browser.switchToWindow(handles[1])
-       postReviewQuestions.validatePostQuestionButton();
-
-       await postReviewQuestions.searchInputField.setValue("Is this product available in blue color?")
-       ASSERT_CHAI.exists(await postReviewQuestions.postQuestion);
-       await postReviewQuestions.popUpValidateFunction();
-       await postReviewQuestions.emailField.waitForDisplayed();
-       await postReviewQuestions.loginFunctionality("opportun.team09@gmail.com","Opportun123");
-       ASSERT_CHAI.exists(await postReviewQuestions.validateQuestionReceived)
-       console.log(await postReviewQuestions.validateQuestionReceived.getText()+"=====================================")
-
-
-
-      
-
-    })
-})
+describe("Post a Review Question on Amazon Product and validate the Same ", async () => {
+  it("Post a Review Question on Amazon Product and validate the Same ", async () => {
+    postReviewQuestions.open();
+    postReviewQuestions.SearchProduct("mobiles");
+    ASSERT_CHAI.exists(await postReviewQuestions.selectMobile);
+    await postReviewQuestions.selectMobile.waitTillInterval(5000);
+    let handles: any = await browser.getWindowHandles();
+    await browser.switchToWindow(handles[1]);
+    postReviewQuestions.ValidatePostQuestionButton();
+    await postReviewQuestions.searchInputField.setValue("Is this product available in blue color?");
+    ASSERT_CHAI.exists(await postReviewQuestions.postQuestion);
+    await postReviewQuestions.PopUpValidateFunction();
+    await postReviewQuestions.emailField.waitForDisplayed();
+    await postReviewQuestions.LoginFunctionality("opportun.team09@gmail.com","Opportun123");
+    ASSERT_CHAI.exists(await postReviewQuestions.validateQuestionReceived);
+    console.log(await postReviewQuestions.validateQuestionReceived.getText());
+  });
+});
