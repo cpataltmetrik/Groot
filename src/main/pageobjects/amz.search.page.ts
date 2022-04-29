f12cb0b487f5d60b47d46df14d453e445e4918ee
cpataltmetrik <102220544+cpataltmetrik@users.noreply.github.com>
2022-04-29 13:38:25 +0530

    added Help and Setting Scenario

--- src/main/pageobjects/amz.search.page.ts
+++ src/main/pageobjects/amz.search.page.ts
@@ -1,49 +1,75 @@
-import { ChainablePromiseElement } from 'webdriverio';
-import Page from './page';
+import { ChainablePromiseElement } from "webdriverio";
+import Page from "./page";
 
 /**
  * sub page containing specific selectors and methods for a specific page
  */
 class SearchPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    public get searchField() {
-        return <any>$('#twotabsearchtextbox');
-    }
+  /**
+   * define selectors using getter methods
+   */
+  public get searchField() {
+    return <any>$("#twotabsearchtextbox");
+  }
 
-    public get btnSearch() {
-        return <any>$('#nav-search-submit-button');
-    }
+  public get btnSearch() {
+    return <any>$("#nav-search-submit-button");
+  }
 
-    public get searchResults() {
-        return <any>$$('//div/h2/a/span[contains(text(),"Apple iPhone 12")]');
-    }
+  public get searchResults() {
+    return <any>$$('//div/h2/a/span[contains(text(),"Apple iPhone 12")]');
+  }
 
-    public get amazonChoiceList() {
-        return <any>$$('//span[text()="Amazon\'s "]/following::span[1][text()="Choice"]');
-    }
+  public get amazonChoiceList() {
+    return <any>(
+      $$('//span[text()="Amazon\'s "]/following::span[1][text()="Choice"]')
+    );
+  }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+  public get allButtonSearch() {
+    return <any>$('//a[@id="nav-hamburger-menu"]//span[text()="All"]');
+  }
 
-    public async searchProduct(productToSearch) {
-        await this.searchField.clickWhenDisplayed();
-        await this.searchField.clickAndhighlight();
-        await this.searchField.setText(productToSearch);
-        await this.btnSearch.clickWhenEnabled();
-    }
+  public get helpAndSettingSection() {
+    return <any>$('//li/div[contains(text(),"help")]');
+  }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+  public get helpAndSettingAllHeadings() {
+    return $$("//div[.='help & settings']/../following-sibling::li/a");
+  }
 
-    public open() {
-        return super.open('');
-    }  
-    
+  /**
+   * a method to encapsule automation code to interact with the page
+   * e.g. to login using username and password
+   */
+
+  public async searchProduct(productToSearch) {
+    await this.searchField.clickWhenDisplayed();
+    await this.searchField.clickAndhighlight();
+    await this.searchField.setText(productToSearch);
+    await this.btnSearch.clickWhenEnabled();
+  }
+
+  public async clickOnSearchAllButton() {
+    await this.allButtonSearch.waitForDisplayed();
+    await this.allButtonSearch.clickWhenDisplayed();
+  }
+
+  public async scrollToHelpAndSettingsSection() {
+    await this.helpAndSettingSection.scrolltoView();
+  }
+
+  public async getHelpAndSettingAllHeadings() {
+    await this.helpAndSettingSection.waitForDisplayed();
+  }
+
+  /**
+   * overwrite specific options to adapt it to page object
+   */
+
+  public open() {
+    return super.open("");
+  }
 }
 
 export default new SearchPage();
