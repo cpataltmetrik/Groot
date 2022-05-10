c4c5d77a29a47265a348a5aed92cdcfca2e24260
balualtimetrik <bpampari@altimetrik.com>
2022-05-10 11:40:49 +0530

    removed unccessary comments

--- test/specs/clientPerformance.spec.ts
+++ test/specs/clientPerformance.spec.ts
@@ -12,19 +12,9 @@ describe('Load Amazon for client performance', () => {
          * capture all metrics in the background
          */
        await SearchPage.open();
-
-        //await browser.url('http://json.org')
-        //browser.pause(10000)
-
-        // let metrics = await browser.getMetrics()
-        // assert.ok(metrics.speedIndex < 1500) // check that speedIndex is below 1.5ms
-
-        // let score = await browser.getPerformanceScore() // get Lighthouse Performance score
-        // console.log("Pefromance Score ===="+score)
-        // //assert.ok(score >= .99) // Lighthouse Performance score is at 99% or higher
-
-        // await $(`//a[contains(text(),'Esperanto')]`).click()
-
+        /**
+         * below code gets the browser metrics and checks for speedindext and performance score
+         */
         let metrics = await browser.getMetrics()
         assert.ok(metrics.speedIndex < 4500)
         let score = await browser.getPerformanceScore()
